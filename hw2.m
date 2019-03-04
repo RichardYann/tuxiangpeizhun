453b15d7634ea956051ff2a1eca286fece933e58
RichardYann <48011716+RichardYann@users.noreply.github.com>
2019-03-05 04:19:01 +0800

    作业代码
    第二次

--- hw2.m
+++ hw2.m
@@ -2,12 +2,9 @@ clear;
 clc;
 ImageA=imread('Image A.jpg');
 ImageB=imread('Image B.jpg');
-subplot(1,2,1);imshow(ImageA);title('Image A');
-subplot(1,2,2);imshow(ImageB);title('Image B');
 cpselect(ImageA,ImageB);
 
-T=cp2tform(fixedPoints,movingPoints,'affine');
-ImageT=imtransform(ImageB,T);
-figure(2);
+T=fitgeotrans(fixedPoints,movingPoints,'affine');
+ImageT=imwarp(ImageB,T);
 subplot(1,2,1);imshow(ImageA);title('Image A');
 subplot(1,2,2);imshow(ImageT);title('Image T');
