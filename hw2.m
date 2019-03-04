e81c002afa6fe8ad2f8214b0ada743159c80e72a
RichardYann <48011716+RichardYann@users.noreply.github.com>
2019-03-05 03:22:27 +0800

    作业代码
    第二次

---
+++ hw2.m
@@ -0,0 +1,13 @@
+clear;
+clc;
+ImageA=imread('Image A.jpg');
+ImageB=imread('Image B.jpg');
+subplot(1,2,1);imshow(ImageA);title('Image A');
+subplot(1,2,2);imshow(ImageB);title('Image B');
+cpselect(ImageA,ImageB);
+
+T=cp2tform(fixedPoints,movingPoints,'affine');
+ImageT=imtransform(ImageB,T);
+figure(2);
+subplot(1,2,1);imshow(ImageA);title('Image A');
+subplot(1,2,2);imshow(ImageT);title('Image T');
